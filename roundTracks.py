2d4c635a2aa9c5c6068de10ed944a438964554c2
procsynth <mail@multoo.eu>
2019-05-08 16:30:50 +0200

    Fix vias disconnection and mixing of tracks of different layer

--- roundTracks.py
+++ roundTracks.py
@@ -21,24 +21,25 @@
 import pcbnew
 import math
 from trackUtils import *
+from pprint import pprint
 
 class flex_round( pcbnew.ActionPlugin ):
  
     def defaults( self ):
 
-        self.name = "Round tracks"
+        self.name = "Tracks rounding"
         self.category = "Modify PCB"
         self.description = "Will round all tracks on the PCB"
 
     def Run( self ):
-        for i in range(3):
+        for i in range(4):
             self.addIntermediateTracks()
     
     def addIntermediateTracks( self ):
 
         # maximum path length is set to .2mm, and progressive shortening is 25% 
-        MAXLENGTH = 0.75
-        SCALING   = 0.25
+        MAXLENGTH = 0.5 # % of length of track used for the arc
+        SCALING   = 0.5  # radius
         SCALE = 1000000.0  
 
         # most queries start with a board
@@ -50,78 +51,90 @@ class flex_round( pcbnew.ActionPlugin ):
         # list off all of the nets in the board.
         for netcode, net in netcodes.items():
             #print("netcode {}, name {}".format(netcode, net.GetNetname()))
-            tracks = board.TracksInNet(net.GetNet()) # get all the tracks in this net
+            allTracks = board.TracksInNet(net.GetNet()) # get all the tracks in this net
+
+            tracksPerLayer = {}
+            # separate track by layer
+            for t in allTracks:
+                layer = t.GetLayer()
+                if t.GetStart() != t.GetEnd(): # ignore vias
+                    if layer not in tracksPerLayer:
+                        tracksPerLayer[layer] = []
+                    tracksPerLayer[layer].append(t)
+
+            for layer in tracksPerLayer:
+                tracks = tracksPerLayer[layer]
+
+                #add all the possible intersections to a unique set, for iterating over later
+                intersections = set();  
+                endings = set();
+                for t1 in range(len(tracks)):
+                    for t2 in range(t1+1, len(tracks)):
+                        #check if these two tracks share an endpoint
+                        # reduce it to a 2-part tuple so there are not multiple objects of the same point in the set
+                        if(tracks[t1].GetLayer() == tracks[t2].GetLayer()):
+                            if(tracks[t1].IsPointOnEnds(tracks[t2].GetStart())): 
+                                intersections.add((tracks[t2].GetStart().x, tracks[t2].GetStart().y))
+                            if(tracks[t1].IsPointOnEnds(tracks[t2].GetEnd())):
+                                intersections.add((tracks[t2].GetEnd().x, tracks[t2].GetEnd().y))
+
+                #for each remaining intersection, shorten each track by the same amount, and place a track between.
+                tracksToAdd = []
+                tracksToShorten = set()
+                for ip in intersections:
+                    (newX, newY) = ip;
+                    intersection = pcbnew.wxPoint(newX, newY)
+                    tracksHere = [];
+                    for t1 in tracks:
+                        # it seems vias are treated as tracks ???? this should take care of that
+                        if(t1.GetLength() > 0):
+                            if similarPoints(t1.GetStart(), intersection):
+                                tracksHere.append(t1)
+                            if similarPoints(t1.GetEnd(), intersection):
+                                #flip track such that all tracks start at the IP
+                                reverseTrack(t1)
+                                tracksHere.append(t1)
 
 
-            #add all the possible intersections to a unique set, for iterating over later
-            intersections = set();  
-            for t1 in range(len(tracks)):
-                for t2 in range(t1+1, len(tracks)):
-                    #check if these two tracks share an endpoint
-                    # reduce it to a 2-part tuple so there are not multiple objects of the same point in the set
-                    if(tracks[t1].IsPointOnEnds(tracks[t2].GetStart())): 
-                        intersections.add((tracks[t2].GetStart().x, tracks[t2].GetStart().y))
-                    if(tracks[t1].IsPointOnEnds(tracks[t2].GetEnd())):
-                        intersections.add((tracks[t2].GetEnd().x, tracks[t2].GetEnd().y))
+                    # sometimes tracksHere is empty ???
+                    if len(tracksHere) == 0:
+                        continue
 
+                    #sort tracks by length, just to find the shortest
+                    tracksHere.sort(key = GetTrackLength)
+                    #shorten all tracks by the same length, which is a function of existing shortest path length
+                    shortenLength = min(tracksHere[0].GetLength() * MAXLENGTH, SCALING*SCALE)
 
+                    #sort these tracks by angle, so new tracks can be drawn between them
+                    tracksHere.sort(key = getTrackAngle)
+                    #shorten all these tracks
+                    for t1 in range(len(tracksHere)):
+                        shortenTrack(tracksHere[t1], shortenLength)
+                    #connect the new startpoints in a circle around the old center point
+                    for t1 in range(len(tracksHere)):
+                        #dont add 2 new tracks in the 2 track case
+                        if not (len(tracksHere) == 2 and t1 == 1):
+                            newPoint1 = cloneWxPoint(tracksHere[t1].GetStart())
+                            newPoint2 = cloneWxPoint(tracksHere[(t1+1)%len(tracksHere)].GetStart())
+                            tracksToAdd.append((newPoint1, newPoint2, tracksHere[t1].GetWidth(), tracksHere[t1].GetLayer(), tracksHere[t1].GetNet()))
 
-            #for each remaining intersection, shorten each track by the same amount, and place a track between.
-            tracksToAdd = set()
-            tracksToShorten = set()
-            for ip in intersections:
-                (newX, newY) = ip;
-                intersection = pcbnew.wxPoint(newX, newY)
-                tracksHere = [];
-                for t1 in tracks:
-                    # it seems vias are treated as tracks ???? this should take care of that
-                    if(t1.GetLength() > 0):
-                        if similarPoints(t1.GetStart(), intersection):
-                            tracksHere.append(t1)
-                        if similarPoints(t1.GetEnd(), intersection):
-                            #flip track such that all tracks start at the IP
-                            reverseTrack(t1)
-                            tracksHere.append(t1)
+                #add all the new tracks in post, so as not to cause problems with set iteration
+                for trackpoints in tracksToAdd:
+                    (sp, ep, width, layer, net) = trackpoints
 
+                    track = pcbnew.TRACK(board)
+                    track.SetStart(sp)
+                    track.SetEnd(ep)
+                    track.SetWidth(width)
+                    track.SetLayer(layer)
+                    board.Add(track)
+                    track.SetNet(net)
 
-                # sometimes tracksHere is empty ???
-                if len(tracksHere) == 0:
-                    continue
-
-                #sort tracks by length, just to find the shortest
-                tracksHere.sort(key = GetTrackLength)
-                #shorten all tracks by the same length, which is a function of existing shortest path length
-                shortenLength = min(tracksHere[0].GetLength() * MAXLENGTH, SCALING*SCALE)
-
-                #sort these tracks by angle, so new tracks can be drawn between them
-                tracksHere.sort(key = getTrackAngle)
-                #shorten all these tracks
-                for t1 in range(len(tracksHere)):
-                    shortenTrack(tracksHere[t1], shortenLength)
-                #connect the new startpoints in a circle around the old center point
-                for t1 in range(len(tracksHere)):
-                    #dont add 2 new tracks in the 2 track case
-                    if not (len(tracksHere) == 2 and t1 == 1):
-                        newPoint1 = cloneWxPoint(tracksHere[t1].GetStart())
-                        newPoint2 = cloneWxPoint(tracksHere[(t1+1)%len(tracksHere)].GetStart())
-                        tracksToAdd.add((newPoint1, newPoint2, tracksHere[t1].GetWidth(), tracksHere[t1].GetLayer(), tracksHere[t1].GetNet()))
-
-            #add all the new tracks in post, so as not to cause problems with set iteration
-            for trackpoints in tracksToAdd:
-                (sp, ep, width, layer, net) = trackpoints
-
-                track = pcbnew.TRACK(board)
-                track.SetStart(sp)
-                track.SetEnd(ep)
-                track.SetWidth(width)
-                track.SetLayer(layer)
-                board.Add(track)
-                track.SetNet(net)
-
-            # the following really fucks with the vias:
-            # remove all tracks of length 0
-            #for track in tracks:
-            #   if track.GetLength() == 0:
-            #       board.Remove(track)
+                # the following really fucks with the vias:
+                # remove all tracks of length 0
+                # for track in tracks:
+                #     print(track.GetLength())
+                #     if track.GetLength() == 1000:
+                #         board.Remove(track)
 
 flex_round().register()
